ba8a20948c8d17c927fa93ba6222fd683c980f3b
Yoshitaka Kuwata <kuwatay@nifty.com>
2019-03-29 08:47:41 +0900

    Update keymap.c

--- keyboards/signum/keymaps/morecat_lab/keymap.c
+++ keyboards/signum/keymaps/morecat_lab/keymap.c
@@ -22,31 +22,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL,  KC_A,     KC_S,    KC_D,   KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,   KC_Z,     KC_X,    KC_C,   KC_V,    KC_B,           KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_ENT, 
+     KC_TAB,   KC_Z,     KC_X,    KC_C,   KC_V,    KC_B,           KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_MINS, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-     LT(LOWER,KC_ENT),KC_LSFT, KC_LALT,KC_LGUI,SFT_T(KC_SPC),LT(RAISE,KC_SPC),LT(RAISE,KC_SPC), SFT_T(KC_SPC),KC_RGUI,KC_RALT, KC_MINS,LT(LOWER,KC_ENT)
+     LT(LOWER,KC_ENT),KC_LSFT, KC_LALT,KC_LGUI,SFT_T(KC_SPC),LT(RAISE,KC_SPC),LT(RAISE,KC_SPC), SFT_T(KC_ENT),KC_RGUI,KC_RALT, KC_RSFT,LT(LOWER,KC_ENT)
   //└────────┴────────┴────────┴────────┴────────┴────────┘      └────────┴────────┴────────┴────────┴────────┴────────┘ 
   ),
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐      ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GESC,  KC_1,    KC_2,     KC_3,   KC_4,    KC_5,           KC_6,     KC_7,   KC_8,    KC_9,    KC_0,   KC_BSPC, 
+     KC_GRAVE, KC_1,    KC_2,     KC_3,   KC_4,    KC_5,           KC_6,     KC_7,   KC_8,    KC_9,    KC_0,   KC_BSPC, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, BL_TOGG, KC_VOLU,         KC_INS, KC_PGUP, KC_LBRC, KC_RBRC, KC_COLN, KC_DQUO, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, BL_STEP, KC_VOLD,         KC_DEL, KC_PGDN, KC_LT,    KC_GT,   KC_QUES, KC_ENT, 
+     KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, BL_STEP, KC_VOLD,         KC_DEL, KC_PGDN, KC_LT,    KC_GT,   KC_QUES, KC_UNDS, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_LSFT, KC_LALT, KC_LGUI, KC_SPC,  KC_LSFT,        KC_RSFT,  KC_SPC, KC_RGUI, KC_RALT, KC_UNDS, KC_TRNS ),
+     KC_TRNS, KC_LSFT, KC_LALT, KC_LGUI, KC_SPC,  KC_LSFT,        KC_RSFT,  KC_SPC, KC_RGUI, KC_RALT, KC_RSFT, KC_TRNS ),
   //└────────┴────────┴────────┴────────┴────────┴────────┘      └────────┴────────┴────────┴────────┴────────┴────────┘ 
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐      ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_BSPC, 
+     KC_TILD, KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
       KC_LCTL, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_BSLS, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_TAB, KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_PSCR,        KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,  KC_DEL, KC_ENT, 
+      KC_TAB, KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_PSCR,        KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,  KC_DEL, KC_UNDS, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_TRNS,KC_LSFT, KC_LALT, KC_LGUI, KC_SPC,  KC_TRNS,        KC_TRNS,  KC_SPC, KC_RGUI, KC_RALT, KC_UNDS, KC_TRNS),
+      KC_TRNS,KC_LSFT, KC_LALT, KC_LGUI, KC_SPC,  KC_TRNS,        KC_TRNS,  KC_SPC, KC_RGUI, KC_RALT, KC_RSFT, KC_TRNS),
   //└────────┴────────┴────────┴────────┴────────┴────────┘      └────────┴────────┴────────┴────────┴────────┴────────┘ 
 
   [_ADJUST] = LAYOUT(
